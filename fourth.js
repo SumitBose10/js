e161d6d3fa158a6d9265f5adc832f29a0aae089a
SumitBose10 <sumitbose260@gmail.com>
2025-07-07 09:36:01 +0530

    practcing

--- fourth.js
+++ fourth.js
@@ -1,24 +1,32 @@
-let languages = {
-    english : "Hello",
-    hindi : "Namaste",
-    bengali : "Nomoskar",
-    spainish : "Hola"
-};
-let languagepreference = prompt("Enter your language preference (english, hindi, bengali, spainish): ");
+// let languages = {
+//     english : "Hello",
+//     hindi : "Namaste",
+//     bengali : "Nomoskar",
+//     spainish : "Hola"
+// };
+// let languagepreference = prompt("Enter your language preference (english, hindi, bengali, spainish): ");
 
-switch(languagepreference) {
-    case "english":
-        console.log(`${languages.english},sumit`);
-        break;
-    case "hindi":
-        console.log(`${languages.hindi},sumit`);
-        break;
-    case "bengali":
-        console.log(`${languages.bengali},sumit`);
-        break;
-    case "spainish":
-        aconsole.log(`${languages.spainish},sumit`);
-        break;
-    default:
-        alert("Language not supported.");
+// switch(languagepreference) {
+//     case "english":
+//         console.log(`${languages.english},sumit`);
+//         break;
+//     case "hindi":
+//         console.log(`${languages.hindi},sumit`);
+//         break;
+//     case "bengali":
+//         console.log(`${languages.bengali},sumit`);
+//         break;
+//     case "spainish":
+//         aconsole.log(`${languages.spainish},sumit`);
+//         break;
+//     default:
+//         alert("Language not supported.");
+// }
+//array
+const marks = [85,97,44,37,76,60];
+let result = 0;
+for (let i in marks){
+    result += marks[i];
 }
+let average = result / marks.length;
+console.log(`The average marks is ${average}`);
