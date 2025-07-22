6f4c9d2eb239c677bb736a478e0818d7d301571a
SumitBose10 <sumitbose260@gmail.com>
2025-07-22 21:31:47 +0530

    promise and promise all

--- third.js
+++ third.js
@@ -32,6 +32,82 @@
 // };
 // alert(`The price of the ${obj.item} is ${obj.price}`);
 //username
-let name = prompt("Enter your name");
-let username =`@${name}${name.length}`;
-console.log("username=",username);
+// let name = prompt("Enter your name");
+// let username =`@${name}${name.length}`;
+// console.log("username=",username);
+// let arr = [1,2,3,4,5,6,7,8];
+// for (let i = 0; i < arr.length; i++) {
+//     setInterval(() => {
+//         console.log(arr[i]);
+//     },3000);
+// }
+// promises
+// function iseven(){
+//     return new Promise((resolve, reject) => {
+//         let number = 9;
+//         if(number%2==0){
+//             resolve("The number is even");
+//         }else{
+//             reject("The number is odd");
+//         }
+//     });
+// }
+// let result = iseven();
+// result.then((message) => {
+//     console.log(message);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+// another promise example
+function fetchdata(){
+    return new Promise((resolve,reject)=>{
+       let fetch = true;
+       if(fetch){
+           console.log("data fetched");
+       }else{
+           console.warn("data is unavialable");
+       }
+    });
+}
+let response = fetchdata();
+response.then((msg)=>{
+    console.log(msg);
+}).catch((error)=>{
+    console.log(error);
+});
+// //prototypial inheritance
+// function person(name){
+//     this.name = name ;
+// }
+// person.prototype.greet = function(){
+//    console.log(`hello, my name is ${this.name}`); 
+// };
+// let p =  new person("Sumit");
+// p.greet();
+// promise and promise all
+function name(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("Sumit");
+        }, 5000);
+    });
+}
+function age(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve(20);
+        }, 7000);
+    });
+}
+async function userdata(params) {
+    try {
+        console.log("Fetching user data...");
+        const [userName, userAge] = await Promise.all([name(), age()]);
+        console.log(`User Name: ${userName}`);
+        console.log(`User Age: ${userAge}`);
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+    }
+}
+userdata();
